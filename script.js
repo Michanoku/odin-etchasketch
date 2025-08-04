6891ca66ca26394309db27bc68c815669a9f3c9d
Michanoku <michael.werker@gmail.com>
2025-08-04 06:54:30 +0000

    Add button to create new grid

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const container = document.querySelector('#container');
-const MAX_WIDTH = 900
-let grid = 16;
+const MAX_SIZE = 900
 
 function sketch(cell) {
     cell.style.backgroundColor = 'slategrey';
 }
 
-document.addEventListener('DOMContentLoaded', () =>  {
+function createGrid(grid) {
+    const cellWidth = 100 / grid;
+    const cellHeight = parseInt(MAX_SIZE / grid);
     for (i = 0; i < (grid * grid); i++ ) {
-        console.log("creating")
-        const cellWidth = 100 / grid;
-        const cellHeight = parseInt(MAX_WIDTH / grid);
-        const cell = document.createElement('div');
-        cell.style.flex = `1 0 ${cellWidth}%`
-        cell.style.height = `${cellHeight}px`; 
-        cell.style.border = '1px solid black';  
-        container.appendChild(cell);
-        cell.addEventListener('mouseover', () => {
-            sketch(cell);
-        });
+        createCell(cellWidth, cellHeight);
     }
+}
+
+function createCell(cellWidth, cellHeight) {
+    const cell = document.createElement('div');
+    cell.style.flex = `1 0 ${cellWidth}%`
+    cell.style.height = `${cellHeight}px`; 
+    cell.style.border = '1px solid black';  
+    container.appendChild(cell);
+    cell.addEventListener('mouseover', () => {
+        sketch(cell);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () =>  {
+    createGrid(16);
 })
