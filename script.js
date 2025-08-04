410bc7186bd268cddb364fc9f0bab6f3871c4227
Michanoku <michael.werker@gmail.com>
2025-08-04 07:40:03 +0000

    Fix check for NaN in prompt
    As it turns out, variable === NaN is always false.
    I looked up a solution and it appears that isNaN(variable) does
    the trick. Now bogus values are rejected properly.

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
+// Initialize nodes and max size of the grid
 const container = document.querySelector('#container');
 const createButton = document.querySelector('#create');
 const MAX_SIZE = 900
 
+// This function is called when a cell is hovered over.
+// It sets an initial color the first time and increases opacity after.
 function sketch(cell) {
     if (!cell.hasAttribute('data-color')) {
+        // Give the color a number between 1-5
         const random = Math.floor(Math.random() * 5);
         cell.setAttribute('data-color', random);
+        // Set initial opacity
         cell.setAttribute('data-opacity', "0.1");
     } else {
+        // Check current opacity and if not at max yet, add more.
         const currentOpacity = parseFloat(cell.dataset.opacity);
         if (currentOpacity != 1) {  
             const newOpacity = currentOpacity + 0.1;
             cell.setAttribute('data-opacity', String(newOpacity));
         }
     }
+    // Create the color with the current opacity and the cells initial color
     switch (cell.dataset.color) {
         case "0":
             cell.style.backgroundColor = `rgba(53, 80, 112, ${cell.dataset.opacity})`;
@@ -33,17 +40,22 @@ function sketch(cell) {
     }
 } 
 
+// This function creates a grid of cells
 function createGrid(grid) {
+    // First remove the old grid
     while (container.hasChildNodes()) {
         container.removeChild(container.firstChild);
     }
+    // Calculate needed sizes for the new cells
     const cellWidth = 100 / grid;
     const cellHeight = parseInt(MAX_SIZE / grid);
+    // Create the grid
     for (i = 0; i < (grid * grid); i++ ) {
         createCell(cellWidth, cellHeight);
     }
 }
 
+// This function creates a cell in the grid, using width and height as provided
 function createCell(cellWidth, cellHeight) {
     const cell = document.createElement('div');
     cell.style.flex = `1 0 ${cellWidth}%`
@@ -56,11 +68,14 @@ function createCell(cellWidth, cellHeight) {
 }
 
 document.addEventListener('DOMContentLoaded', () =>  {
+    // Create initial grid of 16
     createGrid(16);
+    // Event listener for the button to create a new grid
     createButton.addEventListener('click', () => {
+        // Prompt the user for their desired grid size, but make sure no bogus values are entewred
         const userInput = prompt("Enter grid size from 1 to 100:", "16");
         const grid = parseInt(userInput);
-        if (grid === NaN || grid < 1 || grid > 100) {
+        if (isNaN(grid) || grid < 1 || grid > 100) {
             alert("Please enter a number between 1 and 100")
         } else {
             createGrid(grid);
