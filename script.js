468813c5c688c137c4b73e2bb7bcbb13b534b6a9
Michanoku <michael.werker@gmail.com>
2025-08-04 06:13:07 +0000

    Fix math to display flex correctly
    Trying to round numbers made grid width in flex go over 100%, causing
    spill of the wrap items. Solution was letting JavaScript handle
    the calculation without rounding.

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const container = document.querySelector('#container');
+const MAX_WIDTH = 900
 let grid = 16;
 
 document.addEventListener('DOMContentLoaded', () =>  {
     for (i = 0; i < (grid * grid); i++ ) {
         console.log("creating")
-        const cellWidth = Math.round((100 / grid) * 100) / 100;
+        const cellWidth = 100 / grid;
+        const cellHeight = parseInt(MAX_WIDTH / grid);
         const cell = document.createElement('div');
-        cell.style.flex = `1 0 ${cellWidth}%` 
+        cell.style.flex = `1 0 ${cellWidth}%`
+        cell.style.height = `${cellHeight}px`; 
         cell.style.border = '1px solid black';
-
         container.appendChild(cell);
     }
 })
