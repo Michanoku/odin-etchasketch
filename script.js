3d65442ecddc19e5f62c8eead82537ad5eca5cb7
Michanoku <michael.werker@gmail.com>
2025-08-04 05:54:01 +0000

    Create initial grid

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const container = document.querySelector('#container');
 let grid = 16;
 
+document.addEventListener('DOMContentLoaded', () =>  {
+    for (i = 0; i < (grid * grid); i++ ) {
+        console.log("creating")
+        const cellWidth = Math.round((100 / grid) * 100) / 100;
+        const cell = document.createElement('div');
+        cell.style.flex = `1 0 ${cellWidth}%` 
+        cell.style.border = '1px solid black';
 
+        container.appendChild(cell);
+    }
+})
