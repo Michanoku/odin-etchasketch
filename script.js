8dc3ded5a76b95aa97ff0390b4b74652619caf07
Michanoku <michael.werker@gmail.com>
2025-08-04 07:32:19 +0000

    Alter coloring of cells to preserve border
    When using opacity to alter cell opacity, the border would also be
    affected. Using rgba to construct the strings on the backgroundColor
    made it possible to preserve borders.

--- script.js
+++ script.js
@@ -5,43 +5,33 @@ const MAX_SIZE = 900
 function sketch(cell) {
     if (!cell.hasAttribute('data-color')) {
         const random = Math.floor(Math.random() * 5);
-        let color;  
-        switch (random) {
-            case 0:
-                color = '#355070';
-                break;
-            case 1:
-                color = '#6D597A';
-                break;
-            case 2:
-                color = '#B56576';
-                break;
-            case 3:
-                color = '#E56B6F';
-                break;
-            case 4:
-                color = '#EAAC8B';
-                break;
-        }
-        cell.setAttribute('data-color', color);
-        cell.style.backgroundColor = color;
-    }
-    if (!cell.hasAttribute('data-opacity')) {
+        cell.setAttribute('data-color', random);
         cell.setAttribute('data-opacity', "0.1");
     } else {
         const currentOpacity = parseFloat(cell.dataset.opacity);
-        console.log("current");
-        console.log(currentOpacity)
         if (currentOpacity != 1) {  
             const newOpacity = currentOpacity + 0.1;
-            console.log("new")
-            console.log(newOpacity)
             cell.setAttribute('data-opacity', String(newOpacity));
         }
     }
-    console.log(cell.dataset.opacity)
-    cell.style.opacity = cell.dataset.opacity;
-}
+    switch (cell.dataset.color) {
+        case "0":
+            cell.style.backgroundColor = `rgba(53, 80, 112, ${cell.dataset.opacity})`;
+            break;
+        case "1":
+            cell.style.backgroundColor = `rgba(109, 89, 122, ${cell.dataset.opacity})`;
+            break;
+        case "2":
+            cell.style.backgroundColor = `rgba(181, 101, 118, ${cell.dataset.opacity})`;
+            break;
+        case "3":
+            cell.style.backgroundColor = `rgba(229, 107, 111, ${cell.dataset.opacity})`;
+            break;
+        case "4":
+            cell.style.backgroundColor = `rgba(234, 172, 139, ${cell.dataset.opacity})`;
+            break;
+    }
+} 
 
 function createGrid(grid) {
     while (container.hasChildNodes()) {
